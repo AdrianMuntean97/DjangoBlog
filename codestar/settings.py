aea35ae9b8c59d0798a71ce6873ce845779bf412
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-09-04 11:20:18 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["codestar2023-adrian-ba61cc5d1a6e.herokuapp.com"]
 
 
 # Application definition

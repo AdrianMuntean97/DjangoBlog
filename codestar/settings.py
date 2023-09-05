31acaff8e8df396424ef291b4e79aa225ccb52a7
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-09-05 19:15:42 +0000

    code

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-adrianmuntea-djangoblog-7gkrnp9t0kp.ws-eu104.gitpod.io"]
+ALLOWED_HOSTS = ["8000-adrianmuntea-djangoblog-xleccqnogms.ws-eu104.gitpod.io", "localhost"]
 
 
 # Application definition
